3601ef7414cb3f79f8074ba8ce6024020fe851e5
PepiDianabad <petar.y.petrov@elsys-bg.org>
2024-06-11 01:07:33 +0300

    1

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,48 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-import auth
-import messages
+from flask_oidc import OpenIDConnect
+from keycloak import KeycloakOpenID
+from functools import wraps
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/validate-token', methods=['POST'])
-def validate_token():
-    token = request.json.get('token')
-    if not token:
-        return jsonify({"error": "Token is required"}), 400
-    is_valid, data = auth.validate_token(token)
-    if is_valid:
-        return jsonify({"valid": True, "data": data})
-    return jsonify({"valid": False}), 401
+app.config.update({
+    'SECRET_KEY': '0X6Gi6oaFu6Y2nDysARXops5tkdOuEz4',
+    'TESTING': True,
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_RESOURCE_SERVER_ONLY': True,
+    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
+})
 
-@app.route('/messages', methods=['GET', 'POST'])
-def handle_messages():
-    if request.method == 'GET':
-        return jsonify(messages.get_messages())
-    elif request.method == 'POST':
-        content = request.json.get('content')
+oidc = OpenIDConnect(app)
+
+keycloak_openid = KeycloakOpenID(
+    server_url='http://localhost:8080/auth/',
+    client_id='flask-app',
+    realm_name='master2',
+    client_secret_key='0X6Gi6oaFu6Y2nDysARXops5tkdOuEz4'
+)
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
-        is_valid, user_data = auth.validate_token(token)
-        if not is_valid:
-            return jsonify({"error": "Invalid token"}), 401
-        messages.add_message(user_data['preferred_username'], content)
-        return jsonify({"status": "Message added"}), 201
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+
+        try:
+            token = token.split(" ")[1]
+            options = {"verify_signature": True, "verify_aud": True, "verify_exp": True}
+            keycloak_openid.userinfo(token, options=options)
+        except Exception as e:
+            return jsonify({'message': str(e)}), 403
+
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/protected')
+@token_required
+def protected():
+    return jsonify(message="This is a protected route.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
